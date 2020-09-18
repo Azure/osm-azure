fe7bf7ba7abcb5cf5047900ba2ad943ecc95395c
nshankar13 <nshankar@microsoft.com>
2020-09-18 17:20:16 -0400

    changed_version_tag

--- scripts/publish-chart.sh
+++ scripts/publish-chart.sh
@@ -2,7 +2,7 @@
 
 export HELM_EXPERIMENTAL_OCI=1
 
-helm package ./$CHARTNAME --app-version latest
+./helm package --version $IMAGETAG --destination $IMAGEDIR $REPO --debug
 oras push $REGISTRY/$CHARTNAME:$IMAGETAG ./$CHARTNAME-$IMAGETAG.tgz:application/tar+gzip --debug
 
 az acr repository show-manifests --name $REGISTRY --repository $REPO
