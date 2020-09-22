e55869af2a0ecb4b74d4847bd9d2c513f9ac2773
Niranjan Shankar <nshankar@microsoft.com>
2020-09-22 14:28:05 -0400

    modified_script (#3)
    * modified_script
    
    * changed_version_tag

--- scripts/publish-chart.sh
+++ scripts/publish-chart.sh
@@ -2,7 +2,7 @@
 
 export HELM_EXPERIMENTAL_OCI=1
 
-helm chart save $CHARTPATH $REPO:$RELEASE
-helm registry login $REGISTRY
-helm chart push $REPO:$RELEASE
+./helm package --version $IMAGETAG --destination $IMAGEDIR $REPO --debug
+oras push $REGISTRY/$CHARTNAME:$IMAGETAG ./$CHARTNAME-$IMAGETAG.tgz:application/tar+gzip --debug
+
 az acr repository show-manifests --name $REGISTRY --repository $REPO
