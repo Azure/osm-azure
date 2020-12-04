cad0f61b83496fb8fc3f88a2f633feb622576ccd
nshankar13 <nshankar@microsoft.com>
2020-12-04 18:46:14 -0500

    changed workingDirectory for acr cleanup

--- scripts/add-extension.sh
+++ scripts/add-extension.sh
@@ -6,6 +6,7 @@ RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-arc-osm-system}"
 export RESOURCEID=subscriptions/$SUBSCRIPTION/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Kubernetes/connectedClusters/$CLUSTERNAME
 export HELM_EXPERIMENTAL_OCI=1
 
+echo $CHECKOUT_TAG
 jq -n \
     --arg tag "$CHECKOUT_TAG" \
     --arg namespace "$RELEASE_NAMESPACE" \
