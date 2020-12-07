7a4c8e2e60ebeaaaa2b85cd64fc104509986d31c
nshankar13 <nshankar@microsoft.com>
2020-12-07 13:29:50 -0500

    edited add-extension

--- scripts/add-extension.sh
+++ scripts/add-extension.sh
@@ -32,7 +32,3 @@ az rest --method GET --uri "https://management.azure.com/$RESOURCEID/providers/M
 
 # enable the osm extension
 az rest --method PUT --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/osm?api-Version=2020-07-01-preview" --body @osm_extension.json --debug
-
-# confirm chart has been successfully added
-helm ls --all --all-namespaces
-
