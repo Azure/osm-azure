fcac73c1f3394d6d09f7cb429e313245382d6f11
Kalya Subramanian <42158129+ksubrmnn@users.noreply.github.com>
2021-01-11 11:48:16 -0800

    Add delete-extension.sh to clean up osm arc extension (#30)
    Signed-off-by: Kalya Subramanian <kasubra@microsoft.com>

---
+++ scripts/delete-extension.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source .env
+
+export RESOURCEID=subscriptions/$SUBSCRIPTION/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Kubernetes/connectedClusters/$CLUSTERNAME
+EXTENSION_NAME="${EXTENSION_NAME:-osm}"
+API_VERSION="${API_VERSION:-2020-07-01-preview}"
+
+az account set --subscription=$SUBSCRIPTION
+
+# disable the osm extension
+az rest --method DELETE --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/$EXTENSION_NAME?api-Version=$API_VERSION" --debug
