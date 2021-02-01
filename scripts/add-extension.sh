de836a383e7fee9a54bb4a36280f840d91f23611
nshankar13 <nshankar@microsoft.com>
2021-02-01 18:01:28 -0500

    Edited add-extension

--- scripts/add-extension.sh
+++ scripts/add-extension.sh
@@ -4,15 +4,19 @@ source .env
 RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-arc-osm-system}"
 EXTENSION_NAME="${EXTENSION_NAME:-osm}"
 API_VERSION="${API_VERSION:-2020-07-01-preview}"
+EXTENSION_SETTINGS=$1
 
 export RESOURCEID=subscriptions/$SUBSCRIPTION/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Kubernetes/connectedClusters/$CLUSTERNAME
 
 echo $RESOURCEID
 
-jq -n \
-    --arg tag "$CHECKOUT_TAG" \
-    --arg namespace "$RELEASE_NAMESPACE" \
-    '{properties: {extensionType: "Microsoft.openservicemesh", autoUpgradeMinorVersion: "false", version: $tag, releaseTrain: "Staging", scope: { cluster: { releaseNamespace: $namespace } } } }' > osm_extension.json
+if [[ -z "$EXTENSION_SETTINGS" ]]; then
+    EXTENSION_SETTINGS="osm_extension.json"
+    jq -n \
+        --arg tag "$CHECKOUT_TAG" \
+        --arg namespace "$RELEASE_NAMESPACE" \
+        '{properties: {extensionType: "Microsoft.openservicemesh", autoUpgradeMinorVersion: "false", version: $tag, releaseTrain: "Staging", scope: { cluster: { releaseNamespace: $namespace } } } }' > osm_extension.json
+fi
 
 az account set --subscription=$SUBSCRIPTION > /dev/null 2>&1
 
@@ -25,4 +29,4 @@ az resource tag --tags logAnalyticsWorkspaceResourceId=/$RESOURCEID --ids /$RESO
 az rest --method GET --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions?api-Version=$API_VERSION"
 
 # enable the osm extension
-az rest --method PUT --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/$EXTENSION_NAME?api-Version=$API_VERSION" --body @osm_extension.json --debug
+az rest --method PUT --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/$EXTENSION_NAME?api-Version=$API_VERSION" --body @$EXTENSION_SETTINGS --debug
