d7aea98a7460f45a85c0e06555c524ac9f7b01b5
Niranjan Shankar <nshankar@microsoft.com>
2021-02-01 23:17:45 -0500

    Separate enable-arc and osm-installation (#59)
    * Separate enable-arc and osm-installation
    
    * added connectedk8s version
    
    * Granted Permission and Changed Description
    
    * Edited add-extension
    
    * readded extension script
    
    * chmod
    
    * Extra files
    
    * Removed extra files

--- scripts/add-extension.sh
+++ scripts/add-extension.sh
@@ -4,27 +4,21 @@ source .env
 RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-arc-osm-system}"
 EXTENSION_NAME="${EXTENSION_NAME:-osm}"
 API_VERSION="${API_VERSION:-2020-07-01-preview}"
-CONNECTEDK8S_VERSION="${CONNECTEDK8S_VERSION:-0.3.5}"
+EXTENSION_SETTINGS=$1
 
 export RESOURCEID=subscriptions/$SUBSCRIPTION/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Kubernetes/connectedClusters/$CLUSTERNAME
 
 echo $RESOURCEID
 
-jq -n \
-    --arg tag "$CHECKOUT_TAG" \
-    --arg namespace "$RELEASE_NAMESPACE" \
-    '{properties: {extensionType: "Microsoft.openservicemesh", autoUpgradeMinorVersion: "false", version: $tag, releaseTrain: "Staging", scope: { cluster: { releaseNamespace: $namespace } } } }' > osm_extension.json
+if [[ -z "$EXTENSION_SETTINGS" ]]; then
+    EXTENSION_SETTINGS="osm_extension.json"
+    jq -n \
+        --arg tag "$CHECKOUT_TAG" \
+        --arg namespace "$RELEASE_NAMESPACE" \
+        '{properties: {extensionType: "Microsoft.openservicemesh", autoUpgradeMinorVersion: "false", version: $tag, releaseTrain: "Staging", scope: { cluster: { releaseNamespace: $namespace } } } }' > osm_extension.json
+fi
 
 az account set --subscription=$SUBSCRIPTION > /dev/null 2>&1
-
-az extension remove --name connectedk8s
-
-az extension add --source https://shasbextensions.blob.core.windows.net/extensions/connectedk8s-$CONNECTEDK8S_VERSION-py2.py3-none-any.whl -y
-
-az -v 
-
-# enable connected cluster
-az connectedK8s connect -n  $CLUSTERNAME -g $RESOURCEGROUP -l $REGION > /dev/null 2>&1
 
 # confirm
 helm ls --all --all-namespaces
@@ -35,4 +29,4 @@ az resource tag --tags logAnalyticsWorkspaceResourceId=/$RESOURCEID --ids /$RESO
 az rest --method GET --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions?api-Version=$API_VERSION"
 
 # enable the osm extension
-az rest --method PUT --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/$EXTENSION_NAME?api-Version=$API_VERSION" --body @osm_extension.json --debug
+az rest --method PUT --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/$EXTENSION_NAME?api-Version=$API_VERSION" --body @$EXTENSION_SETTINGS --debug
