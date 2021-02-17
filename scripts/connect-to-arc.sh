49ee1f94ac4befb85a17779f05d7e53062e2612d
nshankar13 <nshankar@microsoft.com>
2021-02-17 13:43:56 -0500

    Moved az -v

--- scripts/connect-to-arc.sh
+++ scripts/connect-to-arc.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 az account set --subscription=$SUBSCRIPTION > /dev/null 2>&1
 
-az -v 
-
 # enable connected cluster
 az connectedk8s connect -n  $CLUSTERNAME -g $RESOURCEGROUP -l $REGION > /dev/null 2>&1
