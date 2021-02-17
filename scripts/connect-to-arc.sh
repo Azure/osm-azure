b66e0f64eff8c0d86953e91e1c09be18e51943e3
nshankar13 <nshankar@microsoft.com>
2021-02-17 13:12:47 -0500

    Moved installation out of script

--- scripts/connect-to-arc.sh
+++ scripts/connect-to-arc.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-
-CONNECTEDK8S_VERSION="${CONNECTEDK8S_VERSION:-0.3.5}"
-
 az account set --subscription=$SUBSCRIPTION > /dev/null 2>&1
-
-az extension remove --name connectedk8s
-
-az extension add --source $EXTENSION_LOCATION/connectedk8s-$CONNECTEDK8S_VERSION-py2.py3-none-any.whl -y
 
 az -v 
 
