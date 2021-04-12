b0dc66fba343f30f526ee0ecb1314fef92cdbf4a
nshankar13 <nshankar@microsoft.com>
2021-04-12 18:37:28 -0400

    Added k8s-extension steps and decoupled installation
    Signed-off-by: nshankar13 <nshankar@microsoft.com>

--- scripts/add-extension.sh
+++ scripts/add-extension.sh
@@ -4,38 +4,34 @@ source .env
 
 RELEASE_NAMESPACE="${RELEASE_NAMESPACE:-arc-osm-system}"
 EXTENSION_NAME="${EXTENSION_NAME:-osm}"
-API_VERSION="${API_VERSION:-2020-07-01-preview}"
 EXTENSION_SETTINGS=$1
-
-export RESOURCEID=subscriptions/$SUBSCRIPTION/resourceGroups/$RESOURCEGROUP/providers/Microsoft.Kubernetes/connectedClusters/$CLUSTERNAME
-
-echo "Azure Resource ID: $RESOURCEID"
-
-if [[ -z "$EXTENSION_SETTINGS" ]]; then
-    EXTENSION_SETTINGS="osm_extension.json"
-    jq -n \
-        --arg tag "$EXTENSION_TAG" \
-        --arg namespace "$RELEASE_NAMESPACE" \
-        '{properties: {extensionType: "Microsoft.openservicemesh", autoUpgradeMinorVersion: "false", version: $tag, releaseTrain: "Staging", scope: { cluster: { releaseNamespace: $namespace } }, "configurationProtectedSettings": { "osm.OpenServiceMesh.controllerLogLevel": "debug", } } }' > osm_extension.json
-fi
 
 az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
 
 # confirm
 helm ls --all --all-namespaces
 
-az resource tag \
-   --tags logAnalyticsWorkspaceResourceId="/$RESOURCEID" \
-   --ids "/$RESOURCEID" > /dev/null 2>&1
-
-# get extensions enabled on this cluster
-az rest \
-   --method GET \
-   --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions?api-Version=$API_VERSION"
-
-# enable the osm extension
-az rest \
-   --method PUT \
-   --uri "https://management.azure.com/$RESOURCEID/providers/Microsoft.KubernetesConfiguration/extensions/$EXTENSION_NAME?api-Version=$API_VERSION" \
-   --body @$EXTENSION_SETTINGS \
-   --debug
+if [[ -z "$EXTENSION_SETTINGS" ]]; then
+   az k8s-extension create \
+      --cluster-name $CLUSTERNAME \
+      --resource-group $RESOURCEGROUP \
+      --cluster-type connectedClusters \
+      --extension-type Microsoft.openservicemesh \
+      --scope cluster \
+      --release-train staging \
+      --name $EXTENSION_NAME \
+      --release-namespace $RELEASE_NAMESPACE \
+      --version $EXTENSION_TAG
+else
+   az k8s-extension create \
+      --cluster-name $CLUSTERNAME \
+      --resource-group $RESOURCEGROUP \
+      --cluster-type connectedClusters \
+      --extension-type Microsoft.openservicemesh \
+      --scope cluster \
+      --release-train staging \
+      --name $EXTENSION_NAME \
+      --release-namespace $RELEASE_NAMESPACE \
+      --version $EXTENSION_TAG \
+      --configuration-settings-file $EXTENSION_SETTINGS
+fi
---
+++ scripts/install-k8s-extension.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+K8S_EXTENSION_VERSION="${K8S_EXTENSION_VERSION:-0.2.0}"
+
+az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
+
+az extension remove --name k8s-extension
+
+az extension add --source "$AZ_EXTENSION_LOCATION/k8s-extension-$K8S_EXTENSION_VERSION-py2.py3-none-any.whl" -y
+
+az -v
