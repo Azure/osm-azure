ca925e57fa71ff77b77d3dde2a0ea32e4b5ad566
nshankar13 <nshankar@microsoft.com>
2021-05-04 17:52:34 -0400

    Add index.md

--- scripts/connect-to-arc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
-
-# enable connected cluster
-az connectedk8s connect -n  "$CLUSTERNAME" -g "$RESOURCEGROUP" -l "$REGION" > /dev/null 2>&1
