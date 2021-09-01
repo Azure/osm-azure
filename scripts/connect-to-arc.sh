ce19304998e6e2fd5262f533afcd8e5d74c85900
nshankar13 <nshankar@microsoft.com>
2021-09-01 14:32:53 -0400

    Added helm install
    Signed-off-by: nshankar13 <nshankar@microsoft.com>

--- scripts/connect-to-arc.sh
+++ scripts/connect-to-arc.sh
@@ -2,4 +2,4 @@
 az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
 
 # enable connected cluster
-az connectedk8s connect -n  "$CLUSTERNAME" -g "$RESOURCEGROUP" -l "$REGION"
+az connectedk8s connect -n  "$CLUSTERNAME" -g "$RESOURCEGROUP" -l "$REGION" > /dev/null 2>&1
