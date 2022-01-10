60efdc11bf861e42dc1587e59e94da44e3747ec9
nshankar13 <nshankar@microsoft.com>
2022-01-10 18:27:45 -0500

    Update version to v1.0.0-rc.3
    Signed-off-by: nshankar13 <nshankar@microsoft.com>

--- scripts/connect-to-arc.sh
+++ scripts/connect-to-arc.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
 
+kubectl get pods -A
 # enable connected cluster
-az connectedk8s connect -n  "$CLUSTERNAME" -g "$RESOURCEGROUP" -l "$REGION" > /dev/null 2>&1
+az connectedk8s connect -n  "$CLUSTERNAME" -g "$RESOURCEGROUP" -l "$REGION" --debug
