5a7544e7c030ee41309c721ec159ad66a8500a45
Niranjan Shankar <nshankar@microsoft.com>
2022-11-03 14:27:03 -0400

    Unblock connectedk8s - install python Crypto (#174)
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    Co-authored-by: nshankar <nshankar@microsoft.ghe.com>

--- scripts/connect-to-arc.sh
+++ scripts/connect-to-arc.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
 
+# Needed temporarily to unblock connectedk8s connect step. 
+python -m pip install install Crypto
+
 # enable connected cluster
 az connectedk8s connect -n  "$CLUSTERNAME" -g "$RESOURCEGROUP" -l "$REGION" > /dev/null 2>&1
