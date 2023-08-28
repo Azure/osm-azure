c22536b16c9e92f7ccaf7ee4888cba46f25eb036
Niranjan Shankar <nshankar@microsoft.com>
2023-08-28 14:46:14 -0400

    Update osm-arc version to 1.2.6-beta-1.  (#178)
    * Update OSM-Arc version to v1.2.5-beta-1
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    * Update chart
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    * update upstream repo
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    * Update checkout and agent pools
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    * Update version and fix checkout
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    * Rm unique tag
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    * unpin k8s-extension
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    * Update timeout
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    * Increase timeout
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    
    ---------
    
    Signed-off-by: nshankar <nshankar@microsoft.ghe.com>
    Co-authored-by: nshankar <nshankar@microsoft.ghe.com>

--- scripts/install-k8s-extension.sh
+++ scripts/install-k8s-extension.sh
@@ -4,6 +4,6 @@ az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
 
 az extension remove --name k8s-extension
 
-az extension add --name k8s-extension --version 1.3.5
+az extension add --name k8s-extension
 
 az -v
