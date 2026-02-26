5fdb0611543409f5632215ad432f1828e815a39c
Keith Mattix II <keithmattix@microsoft.com>
2026-02-26 22:33:27 +0000

    Remove the crypto package
    Signed-off-by: Keith Mattix II <keithmattix@microsoft.com>

--- scripts/connect-to-arc.sh
+++ scripts/connect-to-arc.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 az account set --subscription="$SUBSCRIPTION" > /dev/null 2>&1
 
-# Needed temporarily to unblock connectedk8s connect step.
-python3 -m pip install install Crypto
-
 # enable connected cluster
 az connectedk8s connect -n  "$CLUSTERNAME" -g "$RESOURCEGROUP" -l "$REGION" > /dev/null 2>&1
